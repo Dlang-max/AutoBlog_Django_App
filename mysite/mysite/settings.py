6ec17bd027bad5ff8b029a863baf4307d3a2d214
Dlang-max <langd052405@gmail.com>
2024-07-23 13:15:01 -0400

    refactoring settings.py so that all productions checks are met

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:1337', 'https://yourbloggingassistant.com']
 ALLOWED_HOSTS = ['localhost', 'yourbloggingassistant.com']
 
